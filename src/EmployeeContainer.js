be7be3c43b1b5bb2deded0e6976e12d0fadb484d
Alborz Mesbahi <alborz.mesbahi@willowtreeapps.com>
2018-11-02 20:16:59 -0400

    refactor to use props for rendering and state to hide and show employees

--- src/EmployeeContainer.js
+++ src/EmployeeContainer.js
@@ -8,10 +8,14 @@ class EmployeeContainer extends Component {
 
         this.state = {
             employees: [],
-            randomEmployees: []
+            isHidden: true
         }
 
+        this.randomEmployees = []
+        this.randomEmployee = []
+
         this.clickPlay = this.clickPlay.bind(this)
+        this.generateRandomEmployees = this.generateRandomEmployees.bind(this)
     }
 
     clickPlay() {
@@ -23,21 +27,55 @@ class EmployeeContainer extends Component {
                 this.setState({
                     employees: responseData
                 })
+                console.log(this.state)
+                this.generateRandomEmployees()
+                console.log(this.randomEmployees)
+                this.setState({isHidden: false})
             })
             .catch(error => {
                 console.log('Error fetching and parsing requested data', error)
             })
     }
 
+    // generate random list of employees
+    generateRandomEmployees() {
+        // select a random employee
+        this.randomEmployee = this.state.employees[Math.floor(Math.random() * this.state.employees.length)]
+        console.log(this.randomEmployee)
+
+        // create array to hold random employees, including the selected one
+        const randomEmployeesArr = []
+
+        // Add the random employee to the array
+        randomEmployeesArr.push(this.randomEmployee)
+
+        // shuffle employees from state and add 4 into randomEmployees array
+        for (var i = 0; randomEmployeesArr.length <= 4; i++) {
+            let randomIndex = Math.floor(Math.random() * this.state.employees.length),
+                element = this.state.employees.splice(randomIndex, 1)
+            randomEmployeesArr.push(element[0])
+        }
+
+        console.log(randomEmployeesArr)
+
+        // shuffle the randomEmployees array before rendering
+        const shuffledRandomEmployees = []
+
+        for (var j = 0; shuffledRandomEmployees.length <= 4; j++) {
+            let randomIndex = Math.floor(Math.random() * randomEmployeesArr.length),
+                element = randomEmployeesArr.splice(randomIndex, 1)
+            shuffledRandomEmployees.push(element[0])
+        }
+
+        console.log(shuffledRandomEmployees)
+        this.randomEmployees = shuffledRandomEmployees
+    }
+
     render() {
         return (
             <div className="container">
-                <nav>
-                    <ul>
-                        <li onClick={this.clickPlay}>Begin Play</li>
-                    </ul>
-                </nav>
-                <Employees {...this.state} />
+                <button onClick={this.clickPlay}>Begin Play</button>
+                {this.state.isHidden === false ? <Employees {...this.state} randomEmployees={this.randomEmployees} randomEmployee={this.randomEmployee} clickPlay={this.clickPlay} /> : null}
             </div>
         )
     }
--- src/Employees.js
+++ src/Employees.js
@@ -1,48 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Employees = (props) => {
-    // select a random employee
-    const randomEmployee = props.employees[Math.floor(Math.random() * props.employees.length)]
-    console.log(randomEmployee)
-
-    // create array to hold random employees, including the selected one
-    const randomEmployees = []
-
-    // Add the random employee to the array
-    randomEmployees.push(randomEmployee)
-
-    // shuffle employees from state and add 4 into randomEmployees array
-    for (var i = 0; randomEmployees.length <= 4; i++) {
-        let randomIndex = Math.floor(Math.random() * props.employees.length),
-            element = props.employees.splice(randomIndex, 1)
-        randomEmployees.push(element[0])
+class Employees extends Component {
+    render() {
+        return (
+            <div className="cards">
+                {/* Return a random name and ask who this person is */}
+                <h2>Who is {this.props.randomEmployee.firstName} {this.props.randomEmployee.lastName}?</h2>
+                {/*Render shuffled randomEmployees array here*/}
+                {this.props.randomEmployees.map(employee => {
+                    return (
+                        <img src={employee.headshot.url} alt={employee.headshot.alt} key={employee.id}></img>
+                    )
+                })}
+            </div>
+        )
     }
-
-    console.log(randomEmployees)
-
-    // shuffle the randomEmployees array before rendering
-    const shuffledRandomEmployees = []
-
-    for (var j = 0; shuffledRandomEmployees.length <= 4; j++) {
-        let randomIndex = Math.floor(Math.random() * randomEmployees.length),
-            element = randomEmployees.splice(randomIndex, 1)
-        shuffledRandomEmployees.push(element[0])
-    }
-
-    console.log(shuffledRandomEmployees)
-
-    return (
-        <div className="cards">
-            {/* Return a random name and ask who this person is */}
-            {props.employees.length > 0 ? <h2>Who is {randomEmployee.firstName} {randomEmployee.lastName}?</h2> : null}
-            {/*Render shuffled randomEmployees array here*/}
-            {props.employees.map(employee => {
-                return (
-                    <img src={employee.headshot.url} alt={employee.headshot.alt} key={employee.id}></img>
-                )
-            })}
-        </div>
-    )
 }
 
 export default Employees
