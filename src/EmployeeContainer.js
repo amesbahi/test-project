16a2f320a8f809659f8d830637ac2776ba7bf3c5
Alborz Mesbahi <alborz.mesbahi@willowtreeapps.com>
2018-11-03 20:21:25 -0400

    add feature for stat tracking

--- src/EmployeeContainer.js
+++ src/EmployeeContainer.js
@@ -6,34 +6,59 @@ class EmployeeContainer extends Component {
     constructor(props) {
         super(props)
 
+        // Holds the state for employees retreived from API; hidden state for
+        // Employees child until the user clicks the play button; the number of
+        // correct and incorrect guesses made, populated by the updateCorrect
+        // and updateIncorrect methods that are called when the user clicks the
+        // employee image on the child Employees component. Correct and incorrect
+        // state is populated by the correct- and incorrectArray props
         this.state = {
             employees: [],
-            isHidden: true
+            isHidden: true,
+            correct: [],
+            incorrect: []
         }
 
+        // Props to hold the number of correct and incorrect guesses that are
+        // made when the user clicks on the employee image. These are used in 
+        // the updateCorrect and updateIncorrect methods called from the child
+        // component
+        this.correctArray = []
+        this.incorrectArray = []
+
+        // Props to hold a shuffled array of random employees as well as one
+        // singular random employee: the one the user must guess
         this.randomEmployees = []
         this.randomEmployee = []
 
         this.clickPlay = this.clickPlay.bind(this)
+        this.generateOneRandomEmployee = this.generateOneRandomEmployee.bind(this)
         this.generateRandomEmployees = this.generateRandomEmployees.bind(this)
+        this.updateCorrectState = this.updateCorrectState.bind(this)
+        this.updateIncorrectState = this.updateIncorrectState.bind(this)
+    }
+
+    // generate one random employee
+    generateOneRandomEmployee() {
+        // select a random employee
+        this.randomEmployee = this.state.employees[Math.floor(Math.random() * this.state.employees.length)]
+        console.log(this.randomEmployee)
+        return this.randomEmployee
     }
 
     // generate random list of employees
     generateRandomEmployees() {
-        // select a random employee
-        this.randomEmployee = this.state.employees[Math.floor(Math.random() * this.state.employees.length)]
-        console.log(this.randomEmployee)
-
         // create array to hold random employees, including the selected one
         const randomEmployeesArr = []
 
         // Add the random employee to the array
-        randomEmployeesArr.push(this.randomEmployee)
+        randomEmployeesArr.push(this.generateOneRandomEmployee())
 
         // shuffle employees from state and add 4 into randomEmployees array
         for (var i = 0; randomEmployeesArr.length <= 4; i++) {
-            let randomIndex = Math.floor(Math.random() * this.state.employees.length),
-                element = this.state.employees.splice(randomIndex, 1)
+            let randomIndex = Math.floor(Math.random() * this.state.employees.length)
+            let element = this.state.employees.splice(randomIndex, 1)
+            console.log(element)
             randomEmployeesArr.push(element[0])
         }
 
@@ -43,13 +68,29 @@ class EmployeeContainer extends Component {
         const shuffledRandomEmployees = []
 
         for (var j = 0; shuffledRandomEmployees.length <= 4; j++) {
-            let randomIndex = Math.floor(Math.random() * randomEmployeesArr.length),
-                element = randomEmployeesArr.splice(randomIndex, 1)
+            let randomIndex = Math.floor(Math.random() * randomEmployeesArr.length)
+            let element = randomEmployeesArr.splice(randomIndex, 1)
             shuffledRandomEmployees.push(element[0])
         }
 
         console.log(shuffledRandomEmployees)
         this.randomEmployees = shuffledRandomEmployees
+    }
+
+    updateCorrectState() {
+        this.correctArray.push(this.correctArray.length + 1)
+        this.setState({
+            correct: this.correctArray
+        })
+        return this.state.correct.length
+    }
+
+    updateIncorrectState() {
+        this.incorrectArray.push(this.incorrectArray.length + 1)
+        this.setState({
+            incorrect: this.incorrectArray
+        })
+        return this.state.incorrect.length
     }
 
     clickPlay() {
@@ -75,7 +116,7 @@ class EmployeeContainer extends Component {
         return (
             <div className="container">
                 <button onClick={this.clickPlay}>Begin Play</button>
-                {this.state.isHidden === false ? <Employees randomEmployees={this.randomEmployees} randomEmployee={this.randomEmployee} /> : null}
+                {this.state.isHidden === false ? <Employees {...this.state} updateIncorrectState={this.updateIncorrectState} updateCorrectState={this.updateCorrectState} randomEmployees={this.randomEmployees} randomEmployee={this.randomEmployee} /> : null}
             </div>
         )
     }
--- src/Employees.js
+++ src/Employees.js
@@ -13,9 +13,14 @@ class Employees extends Component {
         console.log(e.target)
         console.log(this.props.randomEmployee)
         if (e.target.alt === this.props.randomEmployee.headshot.alt) {
+            // Track how many times the user gets the guess right and wrong
             alert("Correct!")
+            this.props.updateCorrectState()
+            console.log(this.props.correct.length)
         } else {
             alert("Try again!")
+            this.props.updateIncorrectState()
+            console.log(this.props.incorrect.length)
         }
     }
 
@@ -31,10 +36,13 @@ class Employees extends Component {
             <div className="cards">
                 {/* Return a random name and ask who this person is */}
                 <h2>Who is {this.props.randomEmployee.firstName} {this.props.randomEmployee.lastName}?</h2>
+                {/* Render the length of the correct and incorrect state */}
+                <h3>Correct attempts: {this.props.correct.length}</h3>
+                <h3>Incorrect attempts: {this.props.incorrect.length}</h3>
                 {/*Render shuffled randomEmployees array here*/}
-                {this.props.randomEmployees.map(employee => {
+                {this.props.randomEmployees.map((employee, index) => {
                     return (
-                        <img style={style} onClick={this.clickEmployee} src={employee.headshot.url} alt={employee.headshot.alt} key={employee.id}></img>
+                        <img style={style} onClick={this.clickEmployee} src={employee.headshot.url} alt={employee.headshot.alt} key={employee.id+index}></img>
                     )
                 })}
             </div>
